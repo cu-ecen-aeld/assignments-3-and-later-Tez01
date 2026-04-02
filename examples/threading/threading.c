45bd768698a886436198734b5cfaa170365028b0
Tez01 <tejpartap02@gmail.com>
2026-04-01 21:37:42 -0400

    Implement assignment 4 part 1. Working

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,47 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+   
+
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+ 
+    // sleep for n ms
+    int rc = usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if(rc != 0){
+        // didn't sleep well :(
+        ERROR_LOG("Didn't sleep well before obtaining mutex :( RC = %d\n", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // obtain mutex
+    rc = pthread_mutex_lock(thread_func_args->mutex);
+    if(rc != 0){
+        ERROR_LOG("Couldn't obtain mutex! RC = %d\n", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // sleep for n ms
+    rc = usleep(thread_func_args->wait_to_release_ms * 1000);
+    if(rc != 0){
+        // didn't sleep well :(
+        ERROR_LOG("Didn't sleep well after obtaining mutex :( RC = %d\n", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    // release mutex
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if(rc != 0){
+        ERROR_LOG("Couldn't release mutex! RC = %d\n", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    
+    // Everything worked ok
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +69,37 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    if(mutex == NULL){
+        return false;
+    }
+
+
+    // allocate memory for thread_data
+    struct thread_data *data_ptr = malloc(sizeof(struct thread_data));
+    if(data_ptr == NULL){
+        // cannot allocate data
+        ERROR_LOG("Couldn't allocate data.");
+        return false;
+    }
+    
+
+    // setup mutex and wait arguments
+    data_ptr->wait_to_obtain_ms = wait_to_obtain_ms;
+    data_ptr->wait_to_release_ms = wait_to_release_ms;
+    data_ptr->mutex = mutex;
+
+    // create thread
+    int rc = pthread_create(thread,
+                            NULL,
+                            threadfunc,
+                            data_ptr);               
+    if(rc != 0){
+        // cannot create thread
+        ERROR_LOG("Couldn't create thread! RC = %d\n", rc);
+        return false;
+    }
+
+    return true;
 }
 
